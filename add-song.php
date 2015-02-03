b14b374f98bf8f646ee4ba2e3a3dc4c010014324
Karl Campanella-Dysart <karlcd@me.com>
2015-02-02 22:33:08 -0800

    styled

--- add-song.php
+++ add-song.php
@@ -4,66 +4,95 @@ require_once __DIR__ . '/GenreQuery.php';
 require_once __DIR__ . '/ArtistQuery.php';
 require_once __DIR__ . '/Song.php';
 
-if (isset($_POST['submit'])) {
-	$title = $_POST['title'];
-	$artist = $_POST['artist'];
-	$genre = $_POST['genre'];
-	$price = $_POST['price'];
-		
-	$song = new Song();
-
-	if($title !="" && $price!=""){
-		$song->setTitle($title);
-		$song->setArtistId($artist);
-		$song->setGenreId($genre);
-		$song->setPrice($price);
-
-		$song->save();
-
-		?><p>The song <?php echo $song->getTitle() ?>
-   		with an ID of <?php echo $song->getId() ?>
- 		was inserted successfully!</p>
-
- 		<?php
-	}
-
-	session_destroy();
-}
-
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Insert Song</title>
+	<link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
+	<div class="jumbotron">
+		<div class="container"><h2>Insert A Song</h2></div>
+		
+	</div>
+	<div class-"container">
+	<div class="panel panel-default">
+		<div class="panel-body">
+		<form method="post">
+			<div class="form-group">
+				<label>Title: </label>
+				<input type="text" name="title">
+			</div>
+			<div class="form-group">
+				<label>Artist: </label>
+				<select name="artist">
+				<?php 
+					$artistQuery = new ArtistQuery(); 
+					$artists = $artistQuery->getAll();
+				?>
+				<?php foreach($artists as $artistObj) : ?>
+					<option value="<?php echo $artistObj->id ?>"><?php echo $artistObj->artist_name ?></option>
+				<?php endforeach; ?>
+				</select>
+			</div>
+			<div class="form-group">
+				<label>Genre: </label>
+				<select name="genre">
+				<?php 
+					$genreQuery = new GenreQuery(); 
+					$genres = $genreQuery->getAll();
+				?>
+				<?php foreach($genres as $genreObj) : ?>
+					<option value="<?php echo $genreObj->id ?>"><?php echo $genreObj->genre ?></option>
+				<?php endforeach; ?>
+				</select>
+			</div>
+			
+			<div class="form-group">
+				<label>Price: </label>
+				<input type="text" name="price">
+			</div>
+			
+			<input type="submit" name="submit" class="btn btn-default" value="Submit">
+		</form>
+		</div>
+	
+	<?php 
 
-	<form method="post">
-		Title: <input type="text" name="title">
-		Artist:<select name="artist">
-			<?php 
-				$artistQuery = new ArtistQuery(); 
-				$artists = $artistQuery->getAll();
+	if (isset($_POST['submit'])) {
+		$title = $_POST['title'];
+		$artist = $_POST['artist'];
+		$genre = $_POST['genre'];
+		$price = $_POST['price'];
+			
+		$song = new Song();
+
+		if($title !="" && $price!=""){
+			$song->setTitle($title);
+			$song->setArtistId($artist);
+			$song->setGenreId($genre);
+			$song->setPrice($price);
+
+			$song->save();
+
 			?>
-			<?php foreach($artists as $artistObj) : ?>
-				<option value="<?php echo $artistObj->id ?>"><?php echo $artistObj->artist_name ?></option>
-			<?php endforeach; ?>
-		</select>
-		Genre:<select name="genre">
-			<?php 
-				$genreQuery = new GenreQuery(); 
-				$genres = $genreQuery->getAll();
+
+			<div class="panel-footer">
+			    <p>The song <?php echo $song->getTitle() ?>
+	   			with an ID of <?php echo $song->getId() ?>
+	 			was inserted successfully!</p>
+			</div>
+		
+	 		<?php
+		}
+	}
 
 
-			?>
-			<?php foreach($genres as $genreObj) : ?>
-				<option value="<?php echo $genreObj->id ?>"><?php echo $genreObj->genre ?></option>
-			<?php endforeach; ?>
-		</select>
-		Price: <input type="text" name="price">
-		<input type="submit" name="submit" class="btn btn-default" value="Submit">
-	</form>
-
+	 ?>
+	 </div>
+	 </div>
 </body>
 </html>
+
